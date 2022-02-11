e4a483a5bd51a43807fac3108bfeac4c1e50c9f1
SimoneAmari <simone.amari05@gmail.com>
2022-02-11 09:49:51 +0100

    2o Esercizio

---
+++ AmariEsercizio2/AmariEsercizio2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AmariEsercizio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quante parole vuoi analizzare?");
+            int cont = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+            int mediaminore = 0, mediamaggiore = 0;
+            int contmin = 0, contmag = 0;
+
+            while (i < cont)
+            {
+                Console.WriteLine("Inserisci la parola:");
+                string parola = Console.ReadLine();
+
+                int lunghezza = parola.Length;
+
+
+                if (lunghezza <= 8)
+                {
+                    contmin++;
+                    mediaminore = mediaminore + lunghezza;
+                }
+                else
+                {
+                    contmag++;
+                    mediamaggiore = mediamaggiore + lunghezza;
+                }
+
+                i++;
+            }
+            mediaminore = mediaminore / contmin;
+            mediamaggiore = mediamaggiore / contmag;
+            
+            Console.WriteLine("La media delle parole minori di 8 è di: " + mediaminore);
+            Console.WriteLine("La media delle parole maggiori di 8 è di: " + mediamaggiore);
+        }
+    }
+}
