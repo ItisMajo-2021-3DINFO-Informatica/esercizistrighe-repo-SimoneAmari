ccc3e502405d01d207bcbafd84ab5c852a633f88
SimoneAmari <simone.amari05@gmail.com>
2022-02-18 10:17:56 +0100

    3o esercizio

---
+++ AmariEsercizio3/AmariEsercizio3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AmariEsercizio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contoparola = 0;
+            string parola = "";
+            while(parola != "fine")
+            {
+                Console.WriteLine("Inserisci una parola");
+                parola = Console.ReadLine();
+                char primalettera = parola[0];
+                int lunghezza = parola.Length;
+                char ultimalettera = parola[lunghezza - 1];
+                if (ultimalettera == primalettera)
+                {
+                    contoparola++;
+                }
+            }
+            Console.WriteLine("Il numero delle parole che iniziano e finiscono per la stessa lettera è di: "+ contoparola);
+        }
+    }
+}
